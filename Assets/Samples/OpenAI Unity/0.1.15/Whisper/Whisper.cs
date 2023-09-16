008f9c9743c0744bda29c972af8c31a79f4d1ecc
Sophia Sun <53122458+sophiiasun@users.noreply.github.com>
2023-09-16 03:48:12 -0400

    Added voice dictation with proper output

--- Assets/Samples/OpenAI Unity/0.1.15/Whisper/Whisper.cs
+++ Assets/Samples/OpenAI Unity/0.1.15/Whisper/Whisper.cs
@@ -1,5 +1,9 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System;
+using System.Threading;
+using System.Globalization;
+using System.Collections.Generic;
 
 namespace OpenAI
 {
@@ -17,6 +21,8 @@ namespace OpenAI
         private bool isRecording;
         private float time;
         private OpenAIApi openai = new OpenAIApi();
+
+        public Text resultText;
 
         private void Start()
         {
@@ -74,6 +80,65 @@ namespace OpenAI
             progressBar.fillAmount = 0;
             message.text = res.Text;
             recordButton.enabled = true;
+            wordSearch(res.Text);
+        }
+
+        private void wordSearch(string _text) {
+            Dictionary<string, string> unorderedMap = new Dictionary<string, string> {
+                { "c major.", "C major has no sharps or flats." },
+          
+                { "g major.", "G major has one sharp, F#."},
+                { "d major.", "D major has two sharps,  F# and C#."},
+                { "a major.", "A major has three sharps, F#, C#, and G#."},
+                { "e major.", "E major has four sharps, F#, C#, G#, and D#."},
+                { "b major.", "B major has five sharps, F#, C#, G#, D#, and A#."},
+                { "f sharp major.", "F# major has six sharps, F#, C#, G#, D#, A#, and E#."},
+                { "c sharp major.", "C# major has seven sharps, F#, C#, G#, D#, A#, E#, and B#."},
+
+                { "a minor.", "A minor has no sharps or flats." },
+            
+                { "e minor.", "E minor has one sharp, F#."},
+                { "b minor.", "B minor has two sharps,  F# and C#."},
+                { "f sharp minor.", "F# minor has three sharps, F#, C#, and G#."},
+                { "c sharp minor.", "C# minor has four sharps, F#, C#, G#, and D#."},
+                { "g sharp minor.", "G# minor has five sharps, F#, C#, G#, D#, and A#."},
+                { "d sharp minor.", "D# minor has six sharps, F#, C#, G#, D#, A#, and E#."},
+                { "a sharp minor.", "A# minor has seven sharps, F#, C#, G#, D#, A#, E#, and B#."},
+
+                { "f major.", "F major has one flat, B♭."},
+                { "b flat major.", "B♭ major has two flats,  B♭ and E♭."},
+                { "e flat major.", "E♭ major has three flats, B♭, E♭, and A♭."},
+                { "a flat major.", "A♭ major has four flats, B♭, E♭, A♭, and D♭."},
+                { "d flat major.", "D♭ major has five flats, B♭, E♭, A♭, D♭, and G♭."},
+                { "g flat major.", "G♭ major has six flats, B♭, E♭, A♭, D♭, G♭, and C♭."},
+                { "c flat major.", "C♭ major has seven flats, B♭, E♭, A♭, D♭, G♭, C♭, and F♭."},
+
+                { "d minor.", "D minor has one flat, B♭."},
+                { "g minor.", "G minor has two flats,  B♭ and E♭."},
+                { "c minor.", "C minor has three flats, B♭, E♭, and A♭."},
+                { "f minor.", "F minor has four flats, B♭, E♭, A♭, and D♭."},
+                { "b flat minor.", "B♭ minor has five flats, B♭, E♭, A♭, D♭, and G♭."},
+                { "e flat minor.", "E♭ minor has six flats, B♭, E♭, A♭, D♭, G♭, and C♭."},
+                { "a flat minor.", "A♭ minor has seven flats, B♭, E♭, A♭, D♭, G♭, C♭, and F♭."}
+            };
+            _text = _text.ToLower();
+            string[] text = _text.Split(new char[] {' ', '-'}); 
+            int sze = text.Length-1; 
+            string search = text[sze];
+            for (int i = sze - 1; i > 0; i--) {
+                if (text[i].Length >= 1) search = text[i] + " " + search;
+                if (text[i].Length == 1) break;
+            }
+
+
+            // debug
+
+            // string test = "";
+            // for (int i = 0; i < text.Length; i++) test += text[i] + " ";
+            
+            resultText.text = unorderedMap[search];
+
+            // resultText.text = unorderedMap[search];
         }
 
         private void Update()
