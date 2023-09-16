d0929407fc3ec6b40a6f4372bb349a70af7b2d16
Emily <emilywang4321@gmail.com>
2023-09-16 10:35:55 -0400

    Added key prefabs

---
+++ Assets/Scripts/Note.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Note
+{
+    public string pitch;
+    public float startTime;
+    public float endTime;
+}
